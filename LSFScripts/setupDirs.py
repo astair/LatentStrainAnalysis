e43e49f73227b2a51af5a079a43bbda79229595c
Jonas Simon Fleck <Jonas@coffee.wlan.embl.de>
2017-10-12 18:32:56 +0200

    tabs to spaces

--- LSFScripts/setupDirs.py
+++ LSFScripts/setupDirs.py
@@ -6,47 +6,47 @@ import os
 
 # FUNC
 def interface():
-	parser = argparse.ArgumentParser(description="Sets up the directories necessary for the pipeline in <output-dir> and creates the 'SplitInput_ArrayJob.q' script from the input veriables.")
+    parser = argparse.ArgumentParser(description="Sets up the directories necessary for the pipeline in <output-dir> and creates the 'SplitInput_ArrayJob.q' script from the input veriables.")
 
-	parser.add_argument('-1',
-						required=True,
-						dest='READS_1',
-						nargs='+',
-						type=str,
-						metavar='<reads_1>',
-						help='Comma separated list of R1 reads.')    
+    parser.add_argument('-1',
+                        required=True,
+                        dest='READS_1',
+                        nargs='+',
+                        type=str,
+                        metavar='<reads_1>',
+                        help='Comma separated list of R1 reads.')    
 
-	parser.add_argument('-2',
-						required=True,
-						dest='READS_2',
-						nargs='+',
-						type=str,
-						metavar='<reads_2>',
-						help='Comma separated list of R2 reads.')
-	
-	parser.add_argument('-s',
-						dest='SINGLETS',
-						nargs='+',
-						type=str,
-						metavar='<singlets>',
-						help='Comma separated list of unpaired reads.')
+    parser.add_argument('-2',
+                        required=True,
+                        dest='READS_2',
+                        nargs='+',
+                        type=str,
+                        metavar='<reads_2>',
+                        help='Comma separated list of R2 reads.')
+    
+    parser.add_argument('-s',
+                        dest='SINGLETS',
+                        nargs='+',
+                        type=str,
+                        metavar='<singlets>',
+                        help='Comma separated list of unpaired reads.')
 
-	parser.add_argument('-n',
-						dest='n',
-						default='?',
-						type=int,
-						metavar='<sample-num>',
-						help='Number of samples.')
+    parser.add_argument('-n',
+                        dest='n',
+                        default='?',
+                        type=int,
+                        metavar='<sample-num>',
+                        help='Number of samples.')
 
-	parser.add_argument('-o', '--output-dir',
-						required=True,
-						dest='out',
-						type=str,
-						metavar='<output-directory>',
-						help='Output directory for the pipeline.')
+    parser.add_argument('-o', '--output-dir',
+                        required=True,
+                        dest='out',
+                        type=str,
+                        metavar='<output-directory>',
+                        help='Output directory for the pipeline.')
 
-	args = parser.parse_args()
-	return args
+    args = parser.parse_args()
+    return args
 
 
 SplitInput_string = """#!/bin/bash
@@ -76,24 +76,24 @@ echo 'Total time:  '$tdiff' hours'
 
 # MAIN
 if __name__ == "__main__":
-	args = interface()
+    args = interface()
 
-	reads_1 = " ".join(args.READS_1)
-	reads_2 = " ".join(args.READS_2)
-	reads_single = args.SINGLETS
+    reads_1 = " ".join(args.READS_1)
+    reads_2 = " ".join(args.READS_2)
+    reads_single = args.SINGLETS
 
-	output_dir = args.out
-	if not output_dir.endswith('/'):
-		output_dir += '/'
+    output_dir = args.out
+    if not output_dir.endswith('/'):
+        output_dir += '/'
 
-	sample_num = args.n
+    sample_num = args.n
 
-	dir_names = ['Logs','original_reads','hashed_reads','cluster_vectors','read_partitions', 'scripts']
+    dir_names = ['Logs','original_reads','hashed_reads','cluster_vectors','read_partitions', 'scripts']
 
-	for d in dir_names:
-		if not os.path.exists(output_dir + d):
-			os.system('mkdir {0}{1}'.format(output_dir, d))
+    for d in dir_names:
+        if not os.path.exists(output_dir + d):
+            os.system('mkdir {0}{1}'.format(output_dir, d))
 
-	f = open('{0}scripts/SplitInput_ArrayJob.q'.format(output_dir),'w')
-	f.write(SplitInput_string.format(sample_num, output_dir, reads_1, reads_2, reads_single))
-	f.close()
+    f = open('{0}scripts/SplitInput_ArrayJob.q'.format(output_dir),'w')
+    f.write(SplitInput_string.format(sample_num, output_dir, reads_1, reads_2, reads_single))
+    f.close()
