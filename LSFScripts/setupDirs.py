e70f658c332a4b7026eb2137b0a9ed27fe500db0
Jonas Simon Fleck <Jonas@coffee.wlan.embl.de>
2017-10-12 13:58:26 +0200

    setupDirs.py: added argparse, changed string formatting to s.format()

--- LSFScripts/setupDirs.py
+++ LSFScripts/setupDirs.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python
 
-import sys,getopt,os
+import sys
+import getopt
+import os
+
+# FUNC
+def interface():
+    parser = argparse.ArgumentParser(description="Setup the directories necessary for the pipeline in <output-dir>")
+
+    parser.add_argument('-i', '--input-dir',
+    					dest='input',
+                        type=str,
+                        metavar='<input-dir>',
+                        help='Directory with reads to process.')
+
+    parser.add_argument('-n',
+    					dest='n',
+                        type=int,
+                        metavar='<sample-num>',
+                        help='Number of samples.')
+
+    parser.add_argument('-o', '--output-dir',
+                        dest='out',
+                        type=str,
+                        metavar='<output-directory>',
+                        help='Output directory for the pipeline.')
+
+    args = parser.parse_args()
+    return args
+
 
 SplitInput_string = """#!/bin/bash
-#BSUB -J SplitInput[1-%numSamples%]
+#BSUB -J SplitInput[1-{1}]
 #BSUB -o Logs/SplitInput-Out-%I.out
 #BSUB -e Logs/SplitInput-Err-%I.err
 #BSUB -q week
@@ -11,7 +39,7 @@ SplitInput_string = """#!/bin/bash
 echo Date: `date`
 t1=`date +%s`
 sleep ${LSB_JOBINDEX}
-python LSFScripts/array_merge.py -r ${LSB_JOBINDEX} -i %input% -o original_reads/
+python LSFScripts/array_merge.py -r ${LSB_JOBINDEX} -i {2} -o original_reads/
 [ $? -eq 0 ] || echo 'JOB FAILURE: $?'
 echo Date: `date`
 t2=`date +%s`
@@ -19,25 +47,17 @@ tdiff=`echo 'scale=3;('$t2'-'$t1')/3600' | bc`
 echo 'Total time:  '$tdiff' hours'
 """
 
-help_message = "usage example: python setupDirs.py -i /path/to/reads/ -n numberOfSamples"
+# MAIN
 if __name__ == "__main__":
-	try:
-		opts, args = getopt.getopt(sys.argv[1:],'hi:n:',["inputdir="])
-	except:
-		print help_message
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt in ('-h','--help'):
-			print help_message
-			sys.exit()
-		elif opt in ('-i','--inputdir'):
-			inputdir = arg
-			if inputdir[-1] != '/':
-				inputdir += '/'
-		elif opt in ('-n'):
-			n = arg
-	for dir in ['Logs','original_reads','hashed_reads','cluster_vectors','read_partitions']:
-		os.system('mkdir %s' % (dir))
+	args = interface()
+	input_dir = args.input
+	output_dir = args.out
+	sample_num = args.n
+
+	dir_names = ['Logs','original_reads','hashed_reads','cluster_vectors','read_partitions']
+
+	for d in dir_names:
+		os.system('mkdir {0}'.format(d))
 	f = open('LSFScripts/SplitInput_ArrayJob.q','w')
-	f.write(SplitInput_string.replace('%numSamples%',n).replace('%input%',inputdir))
+	f.write(SplitInput_string.format(n, input_dir))
 	f.close()
