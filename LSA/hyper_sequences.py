692ab9587d3667cddbb1bc0c348f483761ecad7d
astair <jonas.simon.fleck@gmail.com>
2017-11-17 15:29:16 +0100

    Rearranged hyper_sequences.py

--- LSA/hyper_sequences.py
+++ LSA/hyper_sequences.py
@@ -10,6 +10,16 @@ class Hyper_Sequences(LSA):
 	def __init__(self,inputpath,outputpath):
 		super(Hyper_Sequences,self).__init__(inputpath,outputpath)
 
+	def generator_to_bins(self,sequence_generator,rc=True):
+		IDs = []
+		hyper_kmers = []
+		test = 0
+		for ID, coords in self.generator_to_coords(sequence_generator):
+			for i in range(len(coords) - self.kmer_size + 1):
+				IDs.append(ID)
+				hyper_kmers.append(coords[i:i + self.kmer_size])
+		return self.coords_to_bins(IDs,hyper_kmers,reverse_compliments=rc)
+
 	def generator_to_coords(self,sequence_generator):
 		for record in sequence_generator:
 			coords = self.letters_to_coords(record)
@@ -22,7 +32,27 @@ class Hyper_Sequences(LSA):
 		ltc = {'A': complex(-1,0),'T': complex(1,0),'C': complex(0,-1),'G': complex(0,1)}
 		return np.array([ltc.get(l, complex(0,0)) for l in S.seq]) * self.quality_to_prob(S.qual)
 
+	def coords_to_bins(self,A,C,reverse_compliments=True):
+		self.get_wheels()
+		num_wheels = self.Wheels[-1]['w'] + 1
+		num_spokes = self.Wheels[-1]['s'] + 1
+		pow2 = np.array([2**j for j in range(num_spokes)])
+		Wc = np.array([w['c'] for w in self.Wheels])
+		C = np.array(C)
+		L = np.dot(C,np.transpose([w['p'] for w in self.Wheels]).conjugate())
+		B = [np.dot((L[:, ws:ws + num_spokes] > Wc[ws:ws + num_spokes]),pow2) for ws in range(0, num_wheels * num_spokes,num_spokes)]
+		if reverse_compliments:
+			L = np.dot(C[:, ::-1] * -1, np.transpose([w['p'] for w in self.Wheels]).conjugate())
+			B2 = [np.dot((L[:, ws:ws + num_spokes] > Wc[ws:ws + num_spokes]),pow2) for ws in range(0, num_wheels * num_spokes,num_spokes)]
+			return A,self.pick_one_from_rc_pair(B,B2)
+		else:
+			return A,B
 
+	# ONLY NEED TO WRITE DOWN ONE KMER FROM REVERSE COMPLIMENT PAIR
+	def pick_one_from_rc_pair(self,b1,b2,mx=1000000):
+		B = [np.array([b1[i],b2[i]]) for i in range(len(b1))]
+		return [b[np.mod(b,mx).argmin(0),range(b.shape[1])] for b in B]
+			
 	def quality_to_prob(self,Q):
 		Q = [self.quality_codes[c] for c in Q]
 		return np.array([1-10**(-q/10.) for q in Q])
@@ -47,36 +77,7 @@ class Hyper_Sequences(LSA):
 		# print(Wheels[0])
 		self.Wheels = [{'w': x[0],'s': x[1],'p': x[2],'c': x[3]} for x in Wheels if (x[0] < wheel_limit) and (x[1] < spoke_limit)]
 
-	def coords_to_bins(self,A,C,reverse_compliments=True):
-		self.get_wheels()
-		num_wheels = self.Wheels[-1]['w'] + 1
-		num_spokes = self.Wheels[-1]['s'] + 1
-		pow2 = np.array([2**j for j in range(num_spokes)])
-		Wc = np.array([w['c'] for w in self.Wheels])
-		C = np.array(C)
-		L = np.dot(C,np.transpose([w['p'] for w in self.Wheels]).conjugate())
-		B = [np.dot((L[:, ws:ws + num_spokes] > Wc[ws:ws + num_spokes]),pow2) for ws in range(0, num_wheels * num_spokes,num_spokes)]
-		if reverse_compliments:
-			L = np.dot(C[:, ::-1] * -1, np.transpose([w['p'] for w in self.Wheels]).conjugate())
-			B2 = [np.dot((L[:, ws:ws + num_spokes] > Wc[ws:ws + num_spokes]),pow2) for ws in range(0, num_wheels * num_spokes,num_spokes)]
-			return A,self.pick_one_from_rc_pair(B,B2)
-		else:
-			return A,B
 
-	# ONLY NEED TO WRITE DOWN ONE KMER FROM REVERSE COMPLIMENT PAIR
-	def pick_one_from_rc_pair(self,b1,b2,mx=1000000):
-		B = [np.array([b1[i],b2[i]]) for i in range(len(b1))]
-		return [b[np.mod(b,mx).argmin(0),range(b.shape[1])] for b in B]
-
-	def generator_to_bins(self,sequence_generator,rc=True):
-		IDs = []
-		hyper_kmers = []
-		test = 0
-		for ID, coords in self.generator_to_coords(sequence_generator):
-			for i in range(len(coords) - self.kmer_size + 1):
-				IDs.append(ID)
-				hyper_kmers.append(coords[i:i + self.kmer_size])
-		return self.coords_to_bins(IDs,hyper_kmers,reverse_compliments=rc)
 
 	def one_wheel(self,w,reads_file):
 		S = []
